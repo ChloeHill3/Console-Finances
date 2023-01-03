c1789d75656c8452f8f7dd89531ac328947c197d
Chloe Hill <chloehill333@gmail.com>
2023-01-03 23:43:30 +0000

    js code cleaned

--- index.js
+++ index.js
@@ -114,14 +114,12 @@ for (var i = 0; i < totalMonths-1 ; i++) {
    var change = (finances[i+1][1] - finances[i][1]);
    totalChange += change;
    avgChange = totalChange/(totalMonths-1);
-
-   var avgChangeRound = Math.round(avgChange * 100)/ 100;
-
+// The greatest increase in profits(date and amount) over the entire period.
    if (greatestInc < change) {
       greatestInc = change;
       greatestIncDate = finances [i+1][0];
    } 
-   
+// The greatest decrease in losses(date and amount) over the entire period.   
    else if (greatestDec > change){
       greatestDec = change;
       greatestDecDate = finances[i+1][0];
@@ -129,42 +127,11 @@ for (var i = 0; i < totalMonths-1 ; i++) {
 
 }
 
-// The greatest increase in profits(date and amount) over the entire period.
-// var greatestDecSum = finances[i] => finances[i].reduce; 
-// greatestDecSum = greatestDecSum/ totalMonths;
-// console.log(greatestDecSum)
+// Logging to Console
 console.log("Financial Analysis")
 console.log("----------------------------");
 console.log ("Total Months: ", totalMonths);
 console.log ("Total: $" + totalProfLoss);
-console.log("Average Change: $" + avgChangeRound);
+console.log("Average Change: $" + avgChange.toFixed(2));
 console.log("Greatest Increase in Profits:" , greatestIncDate, "$" + greatestInc);
 console.log("Greatest Decrease in Profits:" , greatestDecDate, "$" + greatestDec);
-
-// The greatest decrease in losses(date and amount) over the entire period.
-
-  // Total up the differences between each pair of adjoining months & divide by number of array elements
-  // for loop starting with i = 1
-    // Each iteration, subtract the data at position [1] of the previous element from the data at position [1] of the current element
-    // Put that data into a new array variable
-      // So, we need to arr.push() each new 'change' value to the new array
-    // Need a variable to store greatest amount of change (+)
-    // Need a variable to store greatest amount of change (-)
-    // Each iteration, compare the difference to the previous difference
-    // If it's higher than what's already stored in greatest-profit variable, reassign that variable to equal finances[i]
-      // greatest[0] = finances[i][0]
-      // greatest[1] = changeArr[i-1]
-    // If it's lower than what's already stored in greatest-loss variable, reassign that variable to equal finances[i]
-
-  // Add up the total in the new 'changes' array
-    // Need a variable to store the rolling total for this array that's different that the rolling total of profits
-    // for loop to iterate over the changes array
-      // add each element in turn to the rolling total
-
-  // Take that 'total changes amount' variable and divide it by the number of elements in the array
-    // Look up how to limit the answer to two decimal points
-
-// Sample decrementing for loop
-// for (let i = arr.length - 1; i > 0; i--) {
-  
-// }
